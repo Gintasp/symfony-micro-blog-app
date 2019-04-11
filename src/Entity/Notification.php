abbe1207cc21cfb3e2ded9fefdc5cb24e7504f24
Gintautas Plonis <gplonis@gmail.com>
2019-04-11 20:24:16 +0000

    Like notifications model.

---
+++ src/Entity/Notification.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\NotificationRepository")
+ * @ORM\InheritanceType("SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="discr", type="string")
+ * @ORM\DiscriminatorMap({"like"="LikeNotification"})
+ */
+abstract class Notification
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $seen;
+
+    /**
+     * Notification constructor.
+     */
+    public function __construct()
+    {
+        $this->seen = false;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSeen()
+    {
+        return $this->seen;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
